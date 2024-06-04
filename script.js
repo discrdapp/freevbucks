ec3a8654639bc7d450f8416fa0951677e3a8e6a9
LiseQ <121126152+discrdapp@users.noreply.github.com>
2024-06-04 19:39:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     let bialyCzyCzarny = "bialy";
-    let x = 0, y = 0, dx = 10, dy = 10; // Initial position and speed
-    var idiotSong = new Audio('youareanidiotsong.mp3');
+    let x = 0, y = 0, dx = 10, dy = 10;
+    let idiotSong = new Audio('youareanidiotsong.mp3');
+    let youareanidiotarr = [
+        "you are an idiot",
+        "ou are an idiot",
+        "u are an idiot",
+        " are an idiot",
+        "are an idiot",
+        "re an idiot",
+        "e an idiot",
+        " an idiot",
+        "an idiot",
+        "n idiot",
+        " idiot",
+        "idiot",
+        "diot",
+        "iot",
+        "ot",
+        "t",
+    ]
+    let a = 0;
+    let b = 1;
 
     function idiota() {
         for (let i = 1; i <=3; i++) {
@@ -15,11 +35,9 @@ document.addEventListener("DOMContentLoaded", () => {
         let div = document.getElementById("idiotDiv");
 
         if (bialyCzyCzarny == 'bialy') {
-            document.getElementById("favicon").href = 'idiot1.png'
             div.classList.remove("idiotWhite")
             div.classList.add("idiotBlack")
         }  else {
-            document.getElementById("favicon").href = 'idiot2.png'
             div.classList.remove("idiotBlack")
             div.classList.add("idiotWhite")
         }
@@ -30,8 +48,28 @@ document.addEventListener("DOMContentLoaded", () => {
             }  else {
                 bialyCzyCzarny = 'bialy'
             }
-            console.log("xd")
             idiota()
+        }, 120);
+    }
+
+    function title() {
+        if (bialyCzyCzarny == 'bialy') {
+            document.getElementById("favicon").href = 'idiot1.png'
+        }  else {
+            document.getElementById("favicon").href = 'idiot2.png'
+        }
+        if (a >= 0) {
+            document.title = youareanidiotarr[a]
+        }
+        if (a >= youareanidiotarr.length - 1) {
+            b = -b;
+        } else if (a < 0) {
+            b = -b;
+        }
+        a += b
+
+        setTimeout(() => {
+            title()
         }, 100);
     }
 
@@ -46,12 +84,10 @@ document.addEventListener("DOMContentLoaded", () => {
         youareanidiot.document.write("<body style='margin: 0;'><img src='youareanidiot.gif' style='width: 100%; height: 100%;'></body>")
     
         function okienko() {
-            // Move the window
             x += dx;
             y += dy;
             youareanidiot.moveTo(x, y);
     
-            // If the window hits the edge of the screen, reverse direction
             if (x + 400 > window.screen.width || x < 0) dx = -dx;
             if (y + 350 > window.screen.availHeight || y < 0) dy = -dy;
             setTimeout(() => {
@@ -63,8 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 requestAnimationFrame(okienko)
             }, 10);
         }
-    
-        okienko();
+        okienko()
     }
 
     let clicked = false;
@@ -72,17 +107,17 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("click", () => {
         if (!clicked) {
             clicked = true;
-            let legit = document.getElementById("legit")
-            let idiotDiv = document.getElementById("idiotDiv")
-            legit.classList.remove('d-block')
-            legit.classList.add('d-none')
-            idiotDiv.classList.remove('d-none')
-            idiotDiv.classList.add('d-flex')
-            idiota()
+            let legit = document.getElementById("legit");
+            let idiotDiv = document.getElementById("idiotDiv");
+            legit.classList.remove('d-block');
+            legit.classList.add('d-none');
+            idiotDiv.classList.remove('d-none');
+            idiotDiv.classList.add('d-flex');
+            idiota();
+            title();
             idiotSong.play();
 
-            idiotSong.addEventListener("ended", function() 
-            {
+            idiotSong.addEventListener("ended", () => {
                 idiotSong.play();
             });
         }
